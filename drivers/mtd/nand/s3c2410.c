900328807760374f837e2e8c299c01dff7cf65a1
Yauhen Kharuzhy <y.kharuzhy@velesys.com>
2007-12-22 15:30:39 +0200

    Add option for disabling of ECC in S3C2410 NAND driver (compatibility with bug in bootloader)

--- drivers/mtd/nand/s3c2410.c
+++ drivers/mtd/nand/s3c2410.c
@@ -730,6 +730,9 @@ static void s3c2410_nand_init_chip(struct s3c2410_nand_info *info,
 	nmtd->mtd.owner    = THIS_MODULE;
 	nmtd->set	   = set;
 
+#ifdef CONFIG_MTD_NAND_S3C2410_NOECC
+	chip->ecc.mode = NAND_ECC_NONE;
+#else
 	if (hardware_ecc) {
 		chip->ecc.calculate = s3c2410_nand_calculate_ecc;
 		chip->ecc.correct   = s3c2410_nand_correct_data;
@@ -761,6 +764,7 @@ static void s3c2410_nand_init_chip(struct s3c2410_nand_info *info,
 
 	if (set->disable_ecc)
 		chip->ecc.mode	= NAND_ECC_NONE;
+#endif
 }
 
 /* s3c2410_nand_update_chip
