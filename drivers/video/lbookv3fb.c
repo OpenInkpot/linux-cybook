315d0bd6855656c112b8f119894d50c35eaed989
Yauhen Kharuzhy <y.kharuzhy@velesys.com>
2008-02-10 02:11:32 +0200

    Add sysfs attributes to enable/disable manual refresh & partial update

--- drivers/video/lbookv3fb.c
+++ drivers/video/lbookv3fb.c
@@ -22,17 +22,22 @@
 #include <linux/delay.h>
 #include <linux/interrupt.h>
 #include <linux/fb.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/list.h>
+#include <linux/device.h>
+#include <linux/ctype.h>
+
 #include <asm/uaccess.h>
 
 #include <asm/arch/regs-gpio.h>
 #include <asm/hardware.h>
 #include <asm/io.h>
 
-#define debug(fmt, args...) printk(KERN_INFO "%s: " fmt, __FUNCTION__, args) 
-//#define debug(fmt, args...)
+//#define debug(fmt, args...) printk(KERN_INFO "%s: " fmt, __FUNCTION__, args) 
+#define debug(fmt, args...)
 
 /* Apollo controller specific defines */
 #define APOLLO_WRITE_TO_FLASH		0x01
@@ -78,10 +83,17 @@
 #define DPY_W 600
 #define DPY_H 800
 
+struct lbookv3fb_options {
+	unsigned int manual_refresh:1;
+	unsigned int partial_update:1;
+};
+
 struct lbookv3fb_par {
 	struct fb_info *info;
 	struct mutex lock;
 	struct delayed_work deferred_work;
+	struct cdev cdev;
+	struct lbookv3fb_options options;
 };
 
 static struct fb_fix_screeninfo lbookv3fb_fix __devinitdata = {
@@ -267,49 +279,79 @@ static void apollo_reinitialize(void)
 
 #define pack_4pixels(a, b, c, d)	(((a) << 6) | ((b) << 4) |  ((c) << 2) | (d))
 
-static void lbookv3fb_dpy_update(struct lbookv3fb_par *par, int partially_bytes)
+static void lbookv3fb_dpy_update(struct lbookv3fb_par *par)
 {
 	int i;
 	unsigned char *buf = (unsigned char __force *)par->info->screen_base;
-//	int count = DPY_W*DPY_H/8 * par->info->var.bits_per_pixel;
 	int count = par->info->fix.smem_len;
 	unsigned char tmp;
-
-	debug("called\n", par);
-
-	if (partially_bytes)
-		count = partially_bytes;
 
 	cancel_delayed_work(&par->deferred_work);
 	mutex_lock(&par->lock);
 
-//	apollo_send_command(APOLLO_MANUAL_REFRESH);
+	if (par->options.manual_refresh)
+		apollo_send_command(APOLLO_MANUAL_REFRESH);
 
-//	apollo_send_command(APOLLO_LOAD_PARTIAL_PICTURE);
 	apollo_send_command(APOLLO_LOAD_PICTURE);
-
-/*	apollo_send_data(0);
-	apollo_send_data(0);
-
-	apollo_send_data(0);
-	apollo_send_data(0);
-
-	apollo_send_data((600 >> 8) & 0xff);
-	apollo_send_data(600 & 0xff);
-
-	apollo_send_data(((count * 4 / 600) >> 8) & 0xff);
-	apollo_send_data((count * 4 / 600) & 0xff);
-*/
 
 	for (i=0; i < count; i += 4) {
 		tmp = pack_4pixels(buf[i], buf[i+1], buf[i+2], buf[i+3]);
 		apollo_send_data(tmp);
-//		apollo_send_data(buf[i]);
 	}
 
 	apollo_send_command(APOLLO_STOP_LOADING);
-//	apollo_send_command(APOLLO_DISPLAY_PARTIAL_PICTURE);
 	apollo_send_command(APOLLO_DISPLAY_PICTURE);
+
+	mutex_unlock(&par->lock);
+}
+
+/*
+ * x1 must be less than x2, y1 < y2
+ */
+static void lbookv3fb_apollo_update_part(struct lbookv3fb_par *par, 
+		unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2)
+{
+	int i, j, k;
+	unsigned char *buf = (unsigned char __force *)par->info->screen_base;
+	unsigned char t;
+
+	debug("called %p\n", par);
+	debug("x1 = %d, y1 = %d, x2 = %d, y2 = %d, y2 * 600 + x2 = %d\n", x1, y1, x2, y2, y2 * 600 + x2);
+
+	mutex_lock(&par->lock);
+
+	if (par->options.manual_refresh)
+		apollo_send_command(APOLLO_MANUAL_REFRESH);
+
+	apollo_send_command(APOLLO_LOAD_PARTIAL_PICTURE);
+
+	apollo_send_data((x1 >> 8) & 0xff);
+	apollo_send_data(x1 & 0xff);
+
+	apollo_send_data((y1 >> 8) & 0xff);
+	apollo_send_data(y1 & 0xff);
+
+	apollo_send_data((x2 >> 8) & 0xff);
+	apollo_send_data(x2 & 0xff);
+
+	apollo_send_data((y2 >> 8) & 0xff);
+	apollo_send_data(y2 & 0xff);
+
+	k = 0;
+	t = 0;
+	for (i = y1; i <= y2; i++)
+		for (j = x1; j <= x2; j++) {
+			t |= (buf[i * DPY_W + j] << (6 - (k % 4) * 2));
+			k++;
+			if (k % 4 == 0) {
+				apollo_send_data(t);
+				t = 0;
+			}
+		}
+
+	apollo_send_command(APOLLO_STOP_LOADING);
+	apollo_send_command(APOLLO_DISPLAY_PARTIAL_PICTURE);
+
 	mutex_unlock(&par->lock);
 }
 
@@ -317,14 +359,58 @@ static void lbookv3fb_dpy_update(struct lbookv3fb_par *par, int partially_bytes)
 static void lbookv3fb_dpy_deferred_io(struct fb_info *info,
 				struct list_head *pagelist)
 {
-	lbookv3fb_dpy_update(info->par, 0);
+
+	int y1=0, y2=0;
+	struct page *cur;
+	int prev_index = -1;
+	struct lbookv3fb_par *par = info->par;
+
+	if (par->options.partial_update) {
+		list_for_each_entry(cur, pagelist, lru) {
+			debug("cur->index = %d\n", cur->index);
+			if (prev_index == -1) {
+				y1 = cur->index * PAGE_SIZE / DPY_W;
+				y2 = ((cur->index + 1) * PAGE_SIZE - 1) / DPY_W;
+				if (y2 >= DPY_H)
+					y2 = DPY_H - 1;
+				prev_index = cur->index;
+				continue;
+			}
+
+			if (cur->index == prev_index + 1) {
+				y2 = ((cur->index + 1) * PAGE_SIZE - 1) / DPY_W;
+				if (y2 >= DPY_H)
+					y2 = DPY_H - 1;
+				prev_index = cur->index;
+				continue;
+			}
+
+			if (cur->index == prev_index - 1) {
+				y1 = cur->index * PAGE_SIZE / DPY_W;
+				prev_index = cur->index;
+				continue;
+			}
+
+			lbookv3fb_apollo_update_part(info->par, 0, y1, DPY_W - 1, y2);
+			y1 = cur->index * PAGE_SIZE / DPY_W;
+			y2 = ((cur->index + 1) * PAGE_SIZE - 1) / DPY_W;
+			if (y2 >= DPY_H)
+				y2 = DPY_H - 1;
+
+			prev_index = cur->index;
+		}
+		lbookv3fb_apollo_update_part(info->par, 0, y1, DPY_W - 1, y2);
+	} else
+		lbookv3fb_dpy_update(info->par);
 }
 
 static void lbookv3fb_deferred_work(struct work_struct *work)
 {
 	struct lbookv3fb_par *par = container_of(work, struct lbookv3fb_par, deferred_work.work);
 
-	lbookv3fb_dpy_update(par, 0);
+	debug("jiffies= %lu\n", jiffies);
+
+	lbookv3fb_dpy_update(par);
 }
 
 static void lbookv3fb_fillrect(struct fb_info *info,
@@ -419,9 +505,184 @@ static int lbookv3fb_sync(struct fb_info *info)
 {
 	struct lbookv3fb_par *par = info->par;
 
-	lbookv3fb_dpy_update(par, 0);
+//	lbookv3fb_dpy_update(par, 0);
 	return 0;
 }
+
+static ssize_t lbookv3fb_wf_read(struct file *f, char __user *buf, size_t count, loff_t *f_pos)
+{
+	unsigned char data;
+	char __user *p = buf;
+	int i;
+	struct lbookv3fb_par *par = f->private_data;
+
+	mutex_lock(&par->lock);
+
+	if (*f_pos > 1024*1024*2 - 1)
+		return 0;
+
+	if (*f_pos + count > 1024*1024*2)
+		count = 1024*1024*2 - *f_pos;
+
+	for (i = *f_pos; i < *f_pos + count; i++) {
+		apollo_send_command(APOLLO_READ_FROM_FLASH);
+
+		apollo_send_data((i >> 16) & 0xff);
+		apollo_send_data((i >> 8) & 0xff);
+		apollo_send_data(i & 0xff);
+
+		apollo_set_gpa_14_15(1);
+		data = apollo_read_data();
+		apollo_set_gpa_14_15(0);
+
+		if (copy_to_user(p, &data, 1)) {
+			return -EFAULT;
+		}
+		p++;
+	}
+
+	mutex_unlock(&par->lock);
+
+	*f_pos += count;
+	return count;
+}
+
+static ssize_t lbookv3fb_wf_write(struct file *f, const char __user *buf, size_t count, loff_t *f_pos)
+{
+	unsigned char data;
+	const char __user *p = buf;
+	int i;
+	struct lbookv3fb_par *par = f->private_data;
+
+	mutex_lock(&par->lock);
+
+	if (*f_pos > 1024*1024*2 - 1)
+		return 0;
+
+	if (*f_pos + count > 1024*1024*2)
+		count = 1024*1024*2 - *f_pos;
+
+	for (i = *f_pos; i < *f_pos + count; i++) {
+		apollo_send_command(APOLLO_WRITE_TO_FLASH);
+
+		apollo_send_data((i >> 16) & 0xff);
+		apollo_send_data((i >> 8) & 0xff);
+		apollo_send_data(i & 0xff);
+
+		if (copy_to_user(&data, p, 1)) {
+			return -EFAULT;
+		}
+
+		apollo_send_data(data);
+
+		p++;
+	}
+
+	mutex_unlock(&par->lock);
+
+	*f_pos += count;
+	return count;
+}
+
+static int lbookv3fb_wf_open(struct inode *i, struct file *f)
+{
+	struct lbookv3fb_par *par;
+
+	par = container_of(i->i_cdev, struct lbookv3fb_par, cdev);
+
+	f->private_data = par;
+
+	return 0;
+}
+
+static ssize_t lbookv3fb_temperature_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct fb_info *info = dev_get_drvdata(dev);
+	struct lbookv3fb_par *par = info->par;
+	char temp;
+
+	mutex_lock(&par->lock);
+
+	apollo_send_command(APOLLO_READ_TEMPERATURE);
+
+	apollo_set_gpa_14_15(1);
+	temp = apollo_read_data();
+	apollo_set_gpa_14_15(0);
+
+	mutex_unlock(&par->lock);
+
+	sprintf(buf, "%d\n", temp);
+	return strlen(buf) + 1;
+}
+
+static ssize_t lbookv3fb_manual_refresh_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct fb_info *info = dev_get_drvdata(dev);
+	struct lbookv3fb_par *par = info->par;
+
+	sprintf(buf, "%d\n", par->options.manual_refresh);
+	return strlen(buf) + 1;
+}
+
+static ssize_t lbookv3fb_manual_refresh_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
+{
+	struct fb_info *info = dev_get_drvdata(dev);
+	struct lbookv3fb_par *par = info->par;
+	char *after;
+	unsigned long state = simple_strtoul(buf, &after, 10);
+	size_t count = after - buf;
+	ssize_t ret = -EINVAL;
+
+	if (*after && isspace(*after))
+		count++;
+
+	if ((count == size) && (state <= 1)) {
+		ret = count;
+		par->options.manual_refresh = state;
+	}
+
+	return ret;
+}
+
+static ssize_t lbookv3fb_partial_update_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct fb_info *info = dev_get_drvdata(dev);
+	struct lbookv3fb_par *par = info->par;
+
+	sprintf(buf, "%d\n", par->options.partial_update);
+	return strlen(buf) + 1;
+}
+
+static ssize_t lbookv3fb_partial_update_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t size)
+{
+	struct fb_info *info = dev_get_drvdata(dev);
+	struct lbookv3fb_par *par = info->par;
+	char *after;
+	unsigned long state = simple_strtoul(buf, &after, 10);
+	size_t count = after - buf;
+	ssize_t ret = -EINVAL;
+
+	if (*after && isspace(*after))
+		count++;
+
+	if ((count == size) && (state <= 1)) {
+		ret = count;
+		par->options.partial_update = state;
+	}
+
+	return ret;
+}
+
+DEVICE_ATTR(manual_refresh, 0666, lbookv3fb_manual_refresh_show, lbookv3fb_manual_refresh_store);
+DEVICE_ATTR(partial_update, 0666, lbookv3fb_partial_update_show, lbookv3fb_partial_update_store);
+
+static struct file_operations lbookv3fb_wf_fops = {
+	.owner = THIS_MODULE,
+	.open = lbookv3fb_wf_open,
+	.read = lbookv3fb_wf_read,
+	.write = lbookv3fb_wf_write,
+};
+
 
 static struct fb_ops lbookv3fb_ops = {
 	.owner		= THIS_MODULE,
@@ -438,6 +699,41 @@ static struct fb_deferred_io lbookv3fb_defio = {
 	.delay		= HZ,
 	.deferred_io	= lbookv3fb_dpy_deferred_io,
 };
+
+DEVICE_ATTR(temperature, 0444, lbookv3fb_temperature_show, NULL);
+
+
+static int __devinit lbookv3fb_setup_chrdev(struct lbookv3fb_par *par)
+{
+	int res = 0;
+	struct cdev *cdev = &par->cdev;
+	dev_t devno;
+
+	res = alloc_chrdev_region(&devno, 0, 1, "apollo");
+	if (res)
+		goto err_alloc_chrdev_region;
+
+
+	cdev_init(cdev, &lbookv3fb_wf_fops);
+
+	res = cdev_add(cdev, devno, 1);
+	if(res)
+		goto err_cdev_add;
+
+	return 0;
+
+err_cdev_add:
+	unregister_chrdev_region(devno, 1);
+err_alloc_chrdev_region:
+
+	return res;
+}
+
+static void lbookv3fb_remove_chrdev(struct lbookv3fb_par *par)
+{
+	cdev_del(&par->cdev);
+	unregister_chrdev_region(par->cdev.dev, 1);
+}
 
 static int __devinit lbookv3fb_probe(struct platform_device *dev)
 {
@@ -468,6 +764,8 @@ static int __devinit lbookv3fb_probe(struct platform_device *dev)
 	par->info = info;
 	mutex_init(&par->lock);
 	INIT_DELAYED_WORK(&par->deferred_work, lbookv3fb_deferred_work);
+	par->options.manual_refresh = 0;
+	par->options.partial_update = 0;
 
 	info->flags = FBINFO_FLAG_DEFAULT;
 
@@ -494,7 +792,34 @@ static int __devinit lbookv3fb_probe(struct platform_device *dev)
 	       info->node, videomemorysize >> 10);
 
 
+	if ((retval = lbookv3fb_setup_chrdev(par)))
+		goto err2;
+
+	retval = device_create_file(info->dev, &dev_attr_temperature);
+	if(retval)
+		goto err_devattr_temperature;
+
+	retval = device_create_file(info->dev, &dev_attr_manual_refresh);
+	if(retval)
+		goto err_devattr_manref;
+
+	retval = device_create_file(info->dev, &dev_attr_partial_update);
+	if(retval)
+		goto err_devattr_partupd;
+
+
 	return 0;
+
+
+	device_remove_file(info->dev, &dev_attr_partial_update);
+err_devattr_partupd:
+	device_remove_file(info->dev, &dev_attr_manual_refresh);
+err_devattr_manref:
+	device_remove_file(info->dev, &dev_attr_temperature);
+err_devattr_temperature:
+	lbookv3fb_remove_chrdev(par);
+err2:
+	unregister_framebuffer(info);
 err1:
 	framebuffer_release(info);
 err:
@@ -512,8 +837,12 @@ static int __devexit lbookv3fb_remove(struct platform_device *dev)
 		cancel_delayed_work(&par->deferred_work);
 		flush_scheduled_work();
 
+		device_remove_file(info->dev, &dev_attr_temperature);
+		device_remove_file(info->dev, &dev_attr_manual_refresh);
+		device_remove_file(info->dev, &dev_attr_partial_update);
 		unregister_framebuffer(info);
 		vfree((void __force *)info->screen_base);
+		lbookv3fb_remove_chrdev(info->par);
 		framebuffer_release(info);
 	}
 	return 0;
